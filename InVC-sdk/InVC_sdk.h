f04c0ee5057426adb6592118675076a1a16336d4
sowjanyappl <sowjanya@peoplelinkvc.com>
2021-01-22 14:58:04 +0530

    init

---
+++ InVC-sdk/InVC_sdk.h
@@ -0,0 +1,18 @@
+//
+//  InVC_sdk.h
+//  InVC-sdk
+//
+//  Created by Peoplelink on 1/22/21.
+//
+
+#import <Foundation/Foundation.h>
+
+//! Project version number for InVC_sdk.
+FOUNDATION_EXPORT double InVC_sdkVersionNumber;
+
+//! Project version string for InVC_sdk.
+FOUNDATION_EXPORT const unsigned char InVC_sdkVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <InVC_sdk/PublicHeader.h>
+
+
